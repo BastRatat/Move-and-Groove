7aeda1730077e2053a8db1c7318e3285e1e8460f
Bastien <bastien.ratat@gmail.com>
2020-03-20 01:38:12 +0100

    add drop down, remove before_filter

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ApplicationRecord
   belongs_to :user
+  ACTIVITIES = ['Running', 'Walking', 'Cycling', 'Swimming', 'Bodybuilding', 'Rowing', 'Hiking', 'Skating', 'Surfing', 'Climbing']
 end
