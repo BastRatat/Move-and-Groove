b14777076c7b2ee38575ab2e08b88c82e69abca8
Bastien <bastien.ratat@gmail.com>
2020-03-20 16:06:33 +0100

    add conditional to display activity logo

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,11 @@
 class Activity < ApplicationRecord
   belongs_to :user
 
-  ACTIVITIES = ['Running', 'Walking', 'Cycling', 'Swimming', 'Bodybuilding', 'Rowing', 'Hiking', 'Skating', 'Surfing', 'Climbing']
-  DURATION = ['00:15', '00:30', '00:45', '01:00', '01:15', '01:30']
+  ACTIVITIES = ['Running', 'Cycling', 'Ping pong', 'Volleyball', 'Archery', 'Ice-skating', 'Skateboard', 'Surfing', 'Sailing', 'Badminton']
+  DURATION = ['00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00']
+  IMAGES = {
+    'Running': '../../../public/upload/images/running.png',
+    'Surfing': '../../../public/upload/images/surfing.png'
+  }
+
 end
