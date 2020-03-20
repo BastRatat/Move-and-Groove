09df317c8fa2f103df1d8f6d2bab51803899e91f
Bastien <bastien.ratat@gmail.com>
2020-03-20 17:22:55 +0100

    display activity picture for activity choosen by the user

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,9 +3,18 @@ class Activity < ApplicationRecord
 
   ACTIVITIES = ['Running', 'Cycling', 'Ping pong', 'Volleyball', 'Archery', 'Ice-skating', 'Skateboard', 'Surfing', 'Sailing', 'Badminton']
   DURATION = ['00:15', '00:30', '00:45', '01:00', '01:15', '01:30', '01:45', '02:00']
+
   IMAGES = {
-    'Running': '../../../public/upload/images/running.png',
-    'Surfing': '../../../public/upload/images/surfing.png'
+    'Running' => '1',
+    'Cycling' => '2',
+    'Ping pong' => '3',
+    'Volleyball' => '4',
+    'Archery' => '5',
+    'Ice-skating' => '6',
+    'Skateboard' => '7',
+    'Surfing' => '8',
+    'Sailing' => '9',
+    'Badminton' => '10'
   }
 
 end
